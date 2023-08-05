021a94405afb186f62cd9ee24f59ed17402740e4
Federico Lanzani <federico@federicolanzani.com>
2023-08-05 09:38:20 +0200

    :sparkles: Add test app.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Streamlit Starter App
 
 This is a starter app template for Streamlit.
+
+
+To run the application:
+```bash
+streamlit run app/app.py
+```
---
+++ app/app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Starter App"
+)
+
+def main():
+    st.write("Hello World!")
+
+if __name__ == '__main__':
+    main()
